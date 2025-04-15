d0b7a42f5e2bd21a5b63ac289836d47cd08fa5a2
Ailinmino <daianaailin.40@gmail.com>
2025-04-14 21:07:52 -0300

    Create Articulo.cs
    Cree CLASE CATEGORIA
    Agregue los atributos

---
+++ TPWinForm_equipo-11A/TPWinForm_equipo-11A/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_11A
+{
+    internal class Articulo
+    {
+        int ID;
+        string codigo;
+        string nombre;
+        string descripcion;
+        int ID_marca;
+        int ID_categoria;
+        float precio;
+    }
+}
