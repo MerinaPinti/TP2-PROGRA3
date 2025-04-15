285b1b3f770d6bbf64c0de1f453e8a914a9af7aa
Ailinmino <daianaailin.40@gmail.com>
2025-04-14 21:09:52 -0300

    Create Categorias.cs
    Cree CLASE ARTICUL

---
+++ TPWinForm_equipo-11A/TPWinForm_equipo-11A/Categorias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_11A
+{
+    internal class Categorias
+    {
+        int ID;
+        string descripcion;
+    }
+}
