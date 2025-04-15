13a071ee1e4ec71bfcfc2edd86a8346773f1184c
Ailinmino <daianaailin.40@gmail.com>
2025-04-14 21:12:27 -0300

    Create Marcas.cs
    Cree CLASE MARCAS
    Agregue atributos

---
+++ TPWinForm_equipo-11A/TPWinForm_equipo-11A/Marcas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_11A
+{
+    internal class Marcas
+    {
+        int ID;
+        string descripcion;
+    }
+}
