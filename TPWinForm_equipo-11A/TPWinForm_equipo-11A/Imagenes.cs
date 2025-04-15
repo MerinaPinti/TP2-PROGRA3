4d27840f6a22a6019fbc63234066ade34f8ff1c5
Ailinmino <daianaailin.40@gmail.com>
2025-04-14 21:15:47 -0300

    Create Imagenes.cs
    Creo CLASE IMAGENES
    Agrego atributos.

---
+++ TPWinForm_equipo-11A/TPWinForm_equipo-11A/Imagenes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_11A
+{
+    internal class Imagenes
+    {
+        int ID;
+        int ID_articulo;
+        string imagen_URL;
+    }
+}
